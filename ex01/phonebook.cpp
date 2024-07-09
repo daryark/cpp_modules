601f0b021c1a1e977176d87d5f5c1d36f8a73f7b
Dasha Yarkovska <dyarkovs@c2r13s9.42wolfsburg.de>
2024-07-09 17:57:19 +0200

    ex00 done

---
+++ ex01/phonebook.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dyarkovs <dyarkovs@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/09 16:43:32 by dyarkovs          #+#    #+#             */
+/*   Updated: 2024/07/09 17:18:57 by dyarkovs         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "phonebook.hpp"
+
+class phonebook
+{
+private:
+    /* data */
+public:
+    phonebook(/* args */);
+    ~phonebook();
+};
+
+phonebook::phonebook(/* args */)
+{
+}
+
+phonebook::~phonebook()
+{
+}
