925f4e8a2c70ed24931fa343688b23b8df66e6d3
Dasha Yarkovska <dyarkovs@c3r4s3.42wolfsburg.de>
2024-08-19 16:24:10 +0200

    ex05 cpp01 done

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,13 +6,14 @@
 /*   By: dyarkovs <dyarkovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 01:00:59 by dyarkovs          #+#    #+#             */
-/*   Updated: 2024/08/19 14:08:58 by dyarkovs         ###   ########.fr       */
+/*   Updated: 2024/08/19 16:10:36 by dyarkovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <cstdlib>
 
 #define RED "\033[1;31m"
 #define YELLOW "\033[1;33m"
@@ -30,36 +31,36 @@ static bool    printError(std::string type)
     return (true);
 }
 
+static std::string  replaceAll(std::string s, std::string find, std::string replace)
+{
+    std::string out_line = s;
+    size_t pos = out_line.find(find);
+    while (pos != std::string::npos)
+    {
+        out_line = out_line.substr(0, pos) + replace + out_line.substr(pos + find.length());
+        pos = out_line.find(find, pos + replace.length());
+    }
+    return (out_line);
+} 
+
 int main(int ac, char **av)
 {
     if (ac != 4)
         return (printError("structure"));
     std::ifstream in_file(av[1]);
-
     if (!in_file.is_open())
         return (printError("file"));
-    
     std::string s1 = av[2];
     std::string s2 = av[3];
     std::string file = av[1];
-    
     std::string in_line;
-    std::string out_line;
-    size_t pos;
-    
     std::string out_name = file + ".replace";
     std::ofstream out_file(out_name.c_str());
-
     while (std::getline(in_file, in_line))
     {
-        out_line = in_line;
-        pos = out_line.find(s1);
-        while (pos != std::string::npos)
-        {
-            out_line = out_line.substr(0, pos) + s2 + out_line.substr(pos + s1.length());
-            pos = out_line.find(s1, pos + s2.length());
-        }
-        out_file << out_line << std::endl;
+        if (std::cin.eof())
+            exit(0);
+        out_file << replaceAll(in_line, s1, s2) << std::endl;
     }
     out_file.close();
     in_file.close();
