1b62d4682175982aba804844f107886447dc1769
Dasha Yarkovska <dyarkovs@c2r5s5.42wolfsburg.de>
2024-08-26 23:49:33 +0200

    save

---
+++ cpp02/ex03/Point.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Point.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dyarkovs <dyarkovs@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/25 17:10:48 by dyarkovs          #+#    #+#             */
+/*   Updated: 2024/08/26 20:24:32 by dyarkovs         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+class Point
+{
+    private:
+        Fixed const x;
+        Fixed const y;
+    public:
+        Point();
+        Point(const float px, const float py);
+        Point(const Point& other);
+        Point& operator=(const Point& other);
+        ~Point();
+
+        // void        setPoint(); //?!
+        float getY() const;
+        float getX() const;
+};
+    bool bsp( Point const a, Point const b, Point const c, Point const point);
