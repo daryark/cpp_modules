f79310c0832a6e132c6ea89f60d3392b43a7dfa7
Daria Yarkovska <dyarkovs@student.42wolfsburg.de>
2025-01-04 00:23:04 +0100

    cpp06 ex00 done

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -6,63 +6,92 @@
 /*   By: dyarkovs <dyarkovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/27 03:38:41 by dyarkovs          #+#    #+#             */
-/*   Updated: 2025/01/03 00:23:34 by dyarkovs         ###   ########.fr       */
+/*   Updated: 2025/01/04 00:21:41 by dyarkovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ScalarConverter.hpp"
 #include <cctype>
 #include <cstdlib>
+#include <limits> //cpp limits (able to use with templates<>)
+#include <iomanip> //setprecision
+#include <typeinfo> //typeid
+#include "../colors.hpp"
+#include "ScalarConverter.hpp"
 
+//----------------------convertions--------------------------
+template <typename T>
+static void    castToChar(T t)
+{
+	if (t >= 0 && t <= 127)
+	{
+		if (t > 32)
+			std::cout << "'" << static_cast<char>(t) << "'" << std::endl;
+		else
+			std::cout << "Non displayable" << std::endl;
+	}
+	else
+		std::cout << "impossible" << std::endl;
+}
+
+template <typename T, typename C>
+static void	castToNum(T t)
+{
+	if (t == 0 || (t >= std::numeric_limits<C>::min() && t <= std::numeric_limits<C>::max()))
+	{
+		std::cout << std::fixed << std::setprecision(1) << static_cast<C>(t);
+		if (typeid(C) == typeid(float))
+			std::cout << "f"; 
+		std::cout << std::endl;
+	}
+	else
+		std::cout << t << "impossible" << std::endl;
+}
+
+template <typename T>
+static void	printer(T t)
+{
+	std::cout << "char:	";
+	castToChar(t);
+	std::cout << "int:	";
+	castToNum<T, int>(t);
+	std::cout << "float:	";
+	castToNum<T, float>(t);
+	std::cout << "dobule:	";
+	castToNum<T, double>(t);
+}
+
+static bool    isSpecType(std::string s)
+{
+	return s == "+inf" || s == "-inf" || s == "+inff" || s == "-inff" || s == "nan" || s == "nanf";
+}
+
+static void    printSpecType(std::string s)
+{
+	std::cout << "char:	" << "impossible" << std::endl << "int:	" << "impossible" << std::endl;
+	if (s[0] == '+')
+		std::cout << "float:	+inff" << std::endl << "double:	+inf" << std::endl;
+	else if (s[0] == '-')
+		std::cout << "float:	-inff" << std::endl << "double:	-inf" << std::endl;
+	else
+		std::cout << "float:	nanf" << std::endl << "double:	nan" << std::endl;
+}
+
+//--------------------------convertor in class method----------------------------------
 ScalarConverter::~ScalarConverter()
 {
-    std::cout << "DESTRUCTOR" << std::endl;
+	std::cout << "DESTRUCTOR" << std::endl;
 }
 
 void    ScalarConverter::convert(std::string s)
 {
-    std::cout << "CONVERT: " << s << std::endl;
-    if (isSpecType(s))
-    printSpecType();
-    printer("impossible", 2343343434343434, 4.4, 4.4);
-    // std::cout << std::atoi(s) << std::endl;
-}
-
-bool    isSpecType(std::string s)
-{
-    return s == "+inf" || s == "-inf" || s == "+inff" || s == "-inff" || s == "nan" || s == "nanf";
-}
-
-void    convertToChar(s)
-{
-
-}
-
-void    printSpecType(std::string s)
-{
-    char prefix = "";
-    std::string d;
-    std::cout << "char: " << "impossible" << std::endl;
-    std::cout << "int: " << "impossible" << std::endl;
-    if (s[0] == "+" || s[0] == "-")
-    {
-        prefix = s[0];
-        // s = s.substr(1, )
-    }
-    std::cout << "float: " + prefix + std::
-    
-    if (s == "nan" || s == "nanf")
-    {
-        f = "nanf";
-        d = "nan";
-    }
-}
-
-
-void    printer(char c, int i, float f, double d)
-{
-    std::cout << "char: " << c << std::endl;
-    std::cout << "int: " << i << std::endl;
-    std::cout << "float: " << f << std::endl;
-    std::cout << "double: " << d << std::endl;
+	std::cout << YELLOW << "CONVERT:	'" << s << "'" << RE << std::endl;
+	size_t dot = s.find('.');
+	if (isSpecType(s))
+		printSpecType(s);
+	else if (!s[1] && !isdigit(s[0]))
+		printer(static_cast<char>(s[0]));
+	else if (dot == std::string::npos)
+		printer(std::atoi(s.c_str()));
+	else
+		printer(std::atof(s.c_str())); //converts s->double(double size/precise float)
 }
--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: dyarkovs <dyarkovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/27 03:39:09 by dyarkovs          #+#    #+#             */
-/*   Updated: 2025/01/03 00:07:55 by dyarkovs         ###   ########.fr       */
+/*   Updated: 2025/01/04 00:18:07 by dyarkovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,10 @@
 class ScalarConverter
 {
 private:
-    ScalarConverter();//=delete(std11) //*to create static, non-instanciable class
-    ScalarConverter(const ScalarConverter& other);//=delete(std11) //*bcs of requirement for canonical form
-    ScalarConverter&    operator=(const ScalarConverter& other);//=delete(std11) //*bcs of requirement for canonical form
+	ScalarConverter();//=delete(std11) //*to create static, non-instanciable class
+	ScalarConverter(const ScalarConverter& other);//=delete(std11) //*bcs of requirement for canonical form
+	ScalarConverter&    operator=(const ScalarConverter& other);//=delete(std11) //*bcs of requirement for canonical form
 public:
-    ~ScalarConverter();
-    static void convert(std::string s);
+	~ScalarConverter();
+	static void convert(std::string s);
 };
-
-void    printer(char c, int i, float f, double d);
-bool    isSpecType(std::string s);
