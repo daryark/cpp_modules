f9e20837b05469a82e978b59910692abc96b768e
Daria Yarkovska <dyarkovs@student.42wolfsburg.de>
2025-01-05 07:03:52 +0100

    cpp07 done

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,9 +6,106 @@
 /*   By: dyarkovs <dyarkovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 00:20:29 by dyarkovs          #+#    #+#             */
-/*   Updated: 2025/01/05 04:27:00 by dyarkovs         ###   ########.fr       */
+/*   Updated: 2025/01/05 06:54:29 by dyarkovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <iostream>
+
+template <typename T> class Array
+{
+    private:
+        unsigned int    _size;
+        T*              _arr;
+    public:
+        Array(): _size(0), _arr(NULL) {} //creates empty arr
+        Array(unsigned int n): _size(n), _arr(new T[n]) {} //creates arr of n elems
+        ~Array() { delete[] _arr; }
+
+        Array(const Array& other): _size(other.size()), _arr(new T[other.size()])
+        {
+            for (unsigned int i = 0; i < _size; i++)
+                _arr[i] = other._arr[i];
+        }
+        Array& operator=(const Array& other)
+        {
+            if (this != &other)
+            {
+                delete[] _arr;
+                _size = other._size;
+                _arr = new T[_size];
+                for (unsigned int i = 0; i < _size; i++)
+                    _arr[i] = other._arr[i];
+            }
+            return *this;
+        }
+        T&  operator[](unsigned int i)  const
+        {
+            if (i >= _size)
+                throw std::exception();
+            return _arr[i];
+        }
+
+        unsigned int    size() const { return _size; }
+};
+
+//*template can be either written and only used in one .cpp file
+//*or declared and written fully in .hpp file so the linker would be able to use it entirely everywhere
+//*one more option - to write realization separated in .tpp file
+
+// template <typename T>
+// class Array
+// {
+// private:
+//     unsigned int _size;
+//     T* _arr;
+
+// public:
+//     // Default constructor
+//     Array() : _size(0), _arr(NULL) {}
+
+//     // Parameterized constructor
+//     Array(unsigned int n) : _size(n), _arr(new T[n]) {}
+
+//     // Destructor
+//     ~Array() { delete[] _arr; }
+
+//     // Copy constructor - deep copy
+//     Array(const Array& other) : _size(other._size), _arr(new T[other._size])
+//     {
+//         std::cout << "copy\n";
+//         for (unsigned int i = 0; i < _size; i++)
+//         {
+//             _arr[i] = other._arr[i]; // Deep copy elements
+//         }
+//     }
+
+//     // Assignment operator
+//     Array& operator=(const Array& other)
+//     {
+//         if (this != &other) // Self-assignment check
+//         {
+//             std::cout << "operator=\n";
+//             delete[] _arr; // Deallocate existing array
+//             _size = other._size;
+//             _arr = new T[_size]; // Allocate new memory for the copy
+//             for (unsigned int i = 0; i < _size; i++)
+//             {
+//                 _arr[i] = other._arr[i]; // Copy elements from other array
+//             }
+//         }
+//         return *this;
+//     }
+
+//     // Subscript operator
+//     T& operator[](unsigned int i) const
+//     {
+//         if (i >= _size)
+//             throw std::exception(); // Out-of-bounds check
+//         return _arr[i];
+//     }
+
+//     // Size function
+//     unsigned int size() const { return _size; }
+// };
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,12 +6,15 @@
 /*   By: dyarkovs <dyarkovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/08 13:59:58 by dyarkovs          #+#    #+#             */
-/*   Updated: 2025/01/05 00:42:09 by dyarkovs         ###   ########.fr       */
+/*   Updated: 2025/01/05 07:03:08 by dyarkovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <Array.hpp>
+#include <cstdlib> //rand fn
+#include <ctime> //srand fn (gives new arg for rand)
+#include "../colors.hpp"
+#include "Array.hpp"
 
 #define MAX_VAL 750
 int main(int, char**)
@@ -29,7 +32,7 @@ int main(int, char**)
     {
         Array<int> tmp = numbers;
         Array<int> test(tmp);
-    }
+    } //* if the program doesn't core dump here, Then the copy is deap, all clean!
 
     for (int i = 0; i < MAX_VAL; i++)
     {
@@ -61,5 +64,23 @@ int main(int, char**)
         numbers[i] = rand();
     }
     delete [] mirror;//
+
+     // Test default constructor
+    Array<int> a;
+    std::cout << "Size of a: " << a.size() << std::endl;
+
+    // Test constructor with size
+    Array<int> b(5);
+    std::cout << "Size of b: " << b.size() << std::endl;
+    //Fill values
+    for (unsigned int i = 0; i < b.size(); i++)
+        b[i] = i;
+    //copy
+    a = b;
+    //Test operator[] and values
+    for (unsigned int i = 0; i < a.size(); i++)
+        std::cout << MAGENTA << a[i] << RE << std::endl;
+    std::cout << "Size of a: " << a.size() << std::endl;
+    
     return 0;
 }
